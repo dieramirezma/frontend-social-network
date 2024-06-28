27a36c50d8d472b36ef4f80d097d5cac302f58b7
dieramirezma <dieramirezma@unal.edu.co>
2024-06-28 16:40:58 -0500

    feat: Update Sidebar component with user information and counters

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -5,24 +5,27 @@ import { Register } from '../components/user/Register'
 import { PublicLayout } from '../components/layout/public/PublicLayout'
 import { Feed } from '../components/publication/Feed'
 import { Error404 } from '../components/layout/Error404'
+import { AuthProvider } from '../context/AuthProvider'
 
 export const Routing = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route path='/' element={<PublicLayout />}>
-          <Route index element={<Login />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-        </Route>
-        {/* Private Routes */}
-        <Route path='/social' element={<PrivateLayout />}>
-          <Route index element={<Feed />} />
-          <Route path='feed' element={<Feed />} />
-        </Route>
-        <Route path='*' element={<Error404 />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* Public Routes */}
+          <Route path='/' element={<PublicLayout />}>
+            <Route index element={<Login />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+          </Route>
+          {/* Private Routes */}
+          <Route path='/social' element={<PrivateLayout />}>
+            <Route index element={<Feed />} />
+            <Route path='feed' element={<Feed />} />
+          </Route>
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
