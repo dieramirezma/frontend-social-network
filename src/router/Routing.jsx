2932cb6962a6135f091a89c840320f87595a494c
dieramirezma <dieramirezma@unal.edu.co>
2024-06-28 19:40:35 -0500

    feat: Add routes for People and Config components

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -7,6 +7,8 @@ import { Feed } from '../components/publication/Feed'
 import { Error404 } from '../components/layout/Error404'
 import { AuthProvider } from '../context/AuthProvider'
 import { Logout } from '../components/user/Logout'
+import { People } from '../components/user/People'
+import { Config } from '../components/user/Config'
 
 export const Routing = () => {
   return (
@@ -24,6 +26,8 @@ export const Routing = () => {
             <Route index element={<Feed />} />
             <Route path='feed' element={<Feed />} />
             <Route path='logout' element={<Logout />} />
+            <Route path='gente' element={<People />} />
+            <Route path='ajustes' element={<Config />} />
           </Route>
           <Route path='*' element={<Error404 />} />
         </Routes>
